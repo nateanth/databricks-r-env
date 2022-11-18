59fbde05c38050e1083717f965b98cdc967256c5
Nathan Anthony <Nathan.Anthony@databricks.com>
2022-11-18 13:37:40 -0500

    Updated init script to add a snowflake connector

--- init-scripts/create-init-script.py
+++ init-scripts/create-init-script.py
@@ -1,10 +1,10 @@
-# Databricks notebook source
 script = '''#!/bin/bash
 
 # SET VARIABLES
-WORKSPACE_ID=XXXXXXXXXXXXXXXX
-WORKSPACE_URL=XXXXXXXXXXXX.cloud.databricks.com
-MRAN_SNAPSHOT=XXXX-XXX-XX
+WORKSPACE_ID=11111111111111
+WORKSPACE_URL=adb-111111111111.11.azuredatabricks.net
+MRAN_SNAPSHOT=2022-09-08
+SF_ACCOUNT=123123123123123123
 
 if [[ $DB_IS_DRIVER = "TRUE" ]]; then
   
@@ -12,14 +12,21 @@ if [[ $DB_IS_DRIVER = "TRUE" ]]; then
   sudo apt-get update
   sudo apt-get install unixodbc -y
 
-  # install simba spark odbc driver
+  # install simba spark odbc driver [CHANGE FROM https://www.databricks.com/spark/odbc-drivers-archive]
   curl -o \
     /tmp/odbc-driver.zip \
-    https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/odbc/2.6.19/SimbaSparkODBC-2.6.19.1033-Debian-64bit.zip
-
+    https://databricks-bi-artifacts.s3.us-east-2.amazonaws.com/simbaspark-drivers/odbc/2.6.26/SimbaSparkODBC-2.6.26.1045-Debian-64bit.zip
+    
   unzip /tmp/odbc-driver.zip -d /tmp/odbc-simba/
   sudo apt-get install libsasl2-modules-gssapi-mit -y
-  sudo dpkg -i /tmp/odbc-simba/simbaspark_2.6.19.1033-2_amd64.deb
+  sudo dpkg -i /tmp/odbc-simba/simbaspark_2.6.26.1045-2_amd64.deb
+  
+  # install Snowflake odbc driver
+  curl -o \
+  /tmp/sf-odbc-driver.deb \
+  https://sfc-repo.snowflakecomputing.com/odbc/linux/2.25.7/snowflake-odbc-2.25.7.x86_64.deb
+
+  sudo dpkg -i /tmp/sf-odbc-driver.deb
 
   # configure odbc
   echo """
@@ -49,6 +56,15 @@ SSL             = 1
 AuthMech        = 3
 UID             = token
 
+[snowflake]
+Description.    = SnowflakeDB
+Driver          = SnowflakeDSIIDriver
+Locale          = en-US
+SERVER          = ${SF_ACCOUNT}.snowflakecomputing.com
+PORT            = 443
+SSL             = on
+ACCOUNT         = ${SF_ACCOUNT}
+
   """ > /etc/odbc.ini
 
   # configure mlflow
@@ -64,7 +80,7 @@ RETICULATE_PYTHON="/databricks/python3/bin/python3"
   # install mlflow and ODBC as of MRAN snapshot appropriate to DBR
   Rscript -e "install.packages(c('mlflow', 'odbc'), repos='https://cran.microsoft.com/snapshot/${MRAN_SNAPSHOT}/')"
 
-  # RStudio connectio pane configs
+  # RStudio connection pane configs
   mkdir /etc/rstudio/connections
   echo """library(DBI)
 con <- dbConnect(
@@ -83,8 +99,20 @@ con <- dbConnect(
 )
   """ >> /etc/rstudio/connections/'Databricks ODBC.R'
 
+  echo """install.packages(c("DBI", "dplyr","dbplyr","odbc"))
+library(DBI)
+library(dplyr)
+library(dbplyr)
+library(odbc)
+myconn <- DBI::dbConnect(
+  odbc::odbc(), 
+  dsn="snowflake", 
+  uid='\${0:uid=USERNAME}', 
+  pwd='\${1:pwd=Snowflak123}')
+  """ >> /etc/rstudio/connections/'snowflake.R'
+
 fi
 '''
 
-dbutils.fs.mkdirs("/databricks/init")
-dbutils.fs.put("/databricks/init/r-env-init-aws.sh", script, True)
+dbutils.fs.mkdirs("/databricks/init/na")
+dbutils.fs.put("/databricks/init/na/r-env-init-aws.sh", script, True)
